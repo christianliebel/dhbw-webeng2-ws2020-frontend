16b30d81316d4dd6127205bf3e8503fa2f02d656
Christian Liebel <christian.liebel@thinktecture.com>
2020-12-04 09:24:07 +0100

    Labs #53

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -1,3 +1,4 @@
+import { Todo } from './todo';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -6,4 +7,25 @@ import { Injectable } from '@angular/core';
 export class TodoService {
 
   constructor() { }
+
+  create(todo: Todo): Todo {
+    return { id: 1, name: 'Wäsche waschen', done: true };
+  }
+
+  get(todoId: number): Todo {
+    return { id: 1, name: 'Wäsche waschen', done: true };
+  }
+
+  getAll(): Todo[] {
+    return [
+      { id: 1, name: 'Wäsche waschen', done: true },
+      { id: 2, name: 'Straße kehren', done: true },
+      { id: 3, name: 'Aufräumen', done: true }
+    ];
+  }
+
+  update(todo: Todo): void {}
+
+  delete(todoId: number): void {}
+
 }
